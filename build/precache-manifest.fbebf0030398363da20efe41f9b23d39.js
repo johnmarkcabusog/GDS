7753269228a703deab3602aeffa1efb0894d3e1d
johnmarkcabusog <johnmarkcabusog@gmail.com>
2021-02-28 09:43:37 +0800

    add build

---
+++ build/precache-manifest.fbebf0030398363da20efe41f9b23d39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3355d7684e39dc74e831306e59c09120",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4bdda0bd57a0b74f44f",
+    "url": "/static/css/main.e8a87133.chunk.css"
+  },
+  {
+    "revision": "6a463012d8766ae5ac10",
+    "url": "/static/js/2.1f3b967d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.1f3b967d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4bdda0bd57a0b74f44f",
+    "url": "/static/js/main.8f72e2f5.chunk.js"
+  },
+  {
+    "revision": "fb5444777a9652c8c1f7",
+    "url": "/static/js/runtime-main.6585e54a.js"
+  },
+  {
+    "revision": "fd0c41b2a3d127d54284aa93fa708593",
+    "url": "/static/media/aerial-background.fd0c41b2.jpg"
+  },
+  {
+    "revision": "5fcb0054d12066e7e6ce9c132bcedfc5",
+    "url": "/static/media/hello.5fcb0054.png"
+  }
+]);
